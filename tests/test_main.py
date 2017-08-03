2fa0ccfed43cd2e85c7b3430ff134e8949e3775b
Aaron Eikenberry <aeikenberry@gmail.com>
2017-08-03 09:16:21 -0500

    more complete -e test

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,11 @@ import unittest
 from unittest.mock import MagicMock
 
 from pywal import __main__
+from pywal import colors
+from pywal import export
+from pywal import image
 from pywal import reload
+from pywal import sequences
 from pywal.settings import CACHE_DIR
 
 
@@ -19,9 +23,26 @@ class TestMain(unittest.TestCase):
     def test_args_e(self):
         """> Test arg parsing (-e)"""
         reload.env = MagicMock()
-        args = __main__.get_args(["-e"])
+        image.get = MagicMock()
+        colors.get = MagicMock()
+        sequences.send = MagicMock()
+        export.every = MagicMock()
+
+        # Test is not called with -e
+        args = __main__.get_args(["-e", "-i /path.jpg"])
         __main__.process_args(args)
+
+        self.assertTrue(image.get.called)
+        self.assertTrue(colors.get.called)
+        self.assertTrue(sequences.send.called)
         self.assertFalse(reload.env.called)
+
+        # Is called without -e
+        args = __main__.get_args(["-i /path.jpg"])
+        __main__.process_args(args)
+
+        self.assertTrue(reload.env.called)
+
 
 
 if __name__ == "__main__":
